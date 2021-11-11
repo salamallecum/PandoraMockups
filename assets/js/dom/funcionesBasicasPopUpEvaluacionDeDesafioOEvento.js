41d5ccc45225334faf5816fe0c02d8df051b5d32
Lamayat <lamayat@unbosque.edu.co>
2021-11-11 15:55:03 -0500

    AAM - FrontEnd: Se agrega switch para activar o desactivar las actividades academicas

--- assets/js/dom/funcionesBasicasPopUpEvaluacionDeDesafioOEvento.js
+++ assets/js/dom/funcionesBasicasPopUpEvaluacionDeDesafioOEvento.js
@@ -45,18 +45,15 @@ function eventoPopUpDetalleTrabajo(){
 }
 
 //ASIGNACION DE EVENTO A BOTON "EVALUAR" PARA LA EVALUACION DE LAS COMPETENCIAS ESPECÍFICAS
-const listOpen3 = document.getElementsByName('openModal3');
-const modal_container3 = document.getElementById('modal_container3');
-const close3 = document.getElementById('btn_cancelar3');
+var open3 = document.getElementById('openModal3');
+var modal_container3 = document.getElementById('modal_container3');
+var close3 = document.getElementById('btn_cancelar3');
 
-//Recorremos el arreglo de elementos con el name openModal2
-for(var i=0; i<listOpen3.length; i++){
-        
-    listOpen3[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_container3.classList.add('show');
-    },false);
-}
+open3.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal_container3.classList.add('show');
+},false);
+
 
 close3.addEventListener('click', (e) => {
     e.preventDefault();
@@ -64,53 +61,6 @@ close3.addEventListener('click', (e) => {
 },false);
 
 
-//ASIGNACION DE EVENTO A BOTON "DESACTIVAR ACTIVIDAD" PARA LA DESACTIVACION DE LOS DESAFIOS Y EVENTOS
-function eventoPopUpDesactivarActividad(){
-
-    let listOpen4 = document.getElementsByName('openModal4');
-    var modal_container4 = document.getElementById('modal_container4');
-    var close4 = document.getElementById('btn_cancelar4');
-
-    //Recorremos el arreglo de elementos con el name openModal3
-    for(var i=0; i<listOpen4.length; i++){
-        
-        listOpen4[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            modal_container4.classList.add('show');
-        },false);
-    }
-    
-    close4.addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_container4.classList.remove('show');
-    },false); 
-}
-
-//ASIGNACION DE EVENTO A BOTON "ACTIVAR ACTIVIDAD" PARA LA ACTIVACION DE LOS DESAFIOS Y EVENTOS
-function eventoPopUpActivarActividad(){
-
-    let listOpen5 = document.getElementsByName('openModal5');
-    var modal_container5 = document.getElementById('modal_container5');
-    var close5 = document.getElementById('btn_cancelar5');
-
-    //Recorremos el arreglo de elementos con el name openModal3
-    for(var i=0; i<listOpen5.length; i++){
-        
-        listOpen5[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            modal_container5.classList.add('show');
-        },false);
-    }
-    
-    close5.addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_container5.classList.remove('show');
-    },false); 
-}
-
-
 //INVOCACION DE FUNCIONES
 eventoPopUpDetalleDeDesafioOEvento();
 eventoPopUpDetalleTrabajo();
-eventoPopUpDesactivarActividad();
-eventoPopUpActivarActividad();
