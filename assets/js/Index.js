3ffe5af1d7ebb46976dfd2a2d3aa6d7c6e93cbf1
Lamayat <lamayat@unbosque.edu.co>
2021-11-25 16:48:54 -0500

    AAM - FrontEnd: Se construye carrusel con todasu parte funcional (pendiente colocar imgs de las insignias)

--- assets/js/Index.js
+++ assets/js/Index.js
@@ -1,3 +1,5 @@
+import "https://unpkg.com/swiper@7/swiper-bundle.min.js";
+
 
 
 /*===== MENU SHOW =====*/ 
@@ -54,6 +56,41 @@ sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL TEAM DE TRABAJO*/
 sr.reveal('.work__img',{interval: 200}); 
+
+
+//CODIGO DEL CARRUSEL DE BADGES
+const buttonPrev = document.getElementById('button-prev');
+const buttonNext = document.getElementById('button-next');
+const track = document.getElementById('track');
+const slickList = document.getElementById('slick-list');
+const slick = document.querySelectorAll('.slick');
+
+const slickWidth = slick[0].offsetWidth;
+
+buttonPrev.onclick = () => Move(1);
+buttonNext.onclick = () => Move(2);
+
+function Move(value){
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
+    var leftPosition = 0;
+
+    track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+    if(leftPosition < (trackWidth - listWidth) && value == 2){
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }else if(leftPosition > 0 && value == 1){
+        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
