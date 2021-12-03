693bf358632a8e2218f83300118e9c35c40edede
Lamayat <lamayat@unbosque.edu.co>
2021-12-02 21:21:57 -0500

    AAM - FrontEnd: Se reestructuran carpetas y se ajusta interfaces del rol practicas

--- assets/js/dom/funcionesBasicasPopUpConvocatorias.js
+++ assets/js/dom/funcionesBasicasPopUpConvocatorias.js
@@ -64,6 +64,52 @@ function eventoPopUpActualizacionDeConvocatorias(){
     },false); 
 }
 
+//ASIGNACION DE EVENTOS A BOTONES EDITAR PARA LA VISUALIZACIÓN DE CONVOCATORIAS
+function eventoPopUpDetalleDeConvocatorias(){
+
+    let listOpen4 = document.getElementsByName('openModal4');
+    var modal_container4 = document.getElementById('modal_container4');
+    var close4 = document.getElementById('btn_cancelar4');
+
+    //Recorremos el arreglo de elementos con el name openModal2
+    for(var i=0; i<listOpen4.length; i++){
+        
+        listOpen4[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            modal_container4.classList.add('show');
+        },false);
+    }
+    
+    close4.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal_container4.classList.remove('show');
+    },false); 
+}
+
+//ASIGNACION DE EVENTOS A BOTONES COMPARTIR EL EPORTAFOLIO DEL ESTUDIANTE CON EL EMPLEADOR
+function eventoPopUpCompartirEportafolio(){
+
+    let listOpen5 = document.getElementsByName('openModal5');
+    var modal_container5 = document.getElementById('modal_container5');
+    var close5 = document.getElementById('btn_cancelar5');
+
+    //Recorremos el arreglo de elementos con el name openModal2
+    for(var i=0; i<listOpen5.length; i++){
+        
+        listOpen5[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            modal_container5.classList.add('show');
+        },false);
+    }
+    
+    close5.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal_container5.classList.remove('show');
+    },false); 
+}
+
 //INVOCACION DE FUNCIONES
 eventoPopUpActualizacionDeConvocatorias();
 eventoPopUpEliminacionDeConvocatorias();
+eventoPopUpDetalleDeConvocatorias();
+eventoPopUpCompartirEportafolio();
--- assets/js/dom/funcionesBasicasPopUpVisualizacionDeEportafolios.js
+++ assets/js/dom/funcionesBasicasPopUpVisualizacionDeEportafolios.js
@@ -1,30 +1,10 @@
 //Archivo que define las operaciones basicas de los popup en la app Pandora (Abrir y cerrar y dibujarGrafica)
-import "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js";
+//import "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js";
 
 
-//ASIGNACION DE EVENTOS A BOTONES DE DETALLES PARA LA INFORMACIONDE EPORTAFOLIO
-function eventoPopUpInfoEportafolio(){
-
-    let listOpen1 = document.getElementsByName('openModal');
-    var modal_container1 = document.getElementById('modal_container1');
-    var close1 = document.getElementById('btn_cancelar1');
-
-    //Recorremos el arreglo de elementos con el name openModal1
-    for(var i=0; i<listOpen1.length; i++){
-        
-        listOpen1[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            modal_container1.classList.add('show');
-        },false);
-    }
-    
-    close1.addEventListener('click', (e) => {
-        e.preventDefault();
-        modal_container1.classList.remove('show');
-    },false); 
-}
-
+//Va para laplantilla de muestreo del eportafolio
 //FUNCION QUE DIBUJA EL GRAFICO DE PERFILAMIENTO PANDORA EN EL EPORTAFOLIO
+/*
 function dibujarGraficoPerfilamientoPandora(){
 
     var ctx = document.getElementById("grafPerfPandora");
@@ -44,27 +24,33 @@ function dibujarGraficoPerfilamientoPandora(){
         },
     });
 }
+*/
 
-//ASIGNACION DE EVENTOS AL POPUP PARA COMPARTIR UN E-PORTAFOLIO
-const open2 = document.getElementById('openModal2');
-const modal_container2 = document.getElementById('modal_container2');
-const close2 = document.getElementById('btn_cancelar2');
+//ASIGNACION DE EVENTOS A BOTONES COMPARTIR EL EPORTAFOLIO DEL ESTUDIANTE CON EL EMPLEADOR
+function eventoPopUpCompartirEportafolio(){
 
+    let listOpen2 = document.getElementsByName('openModal2');
+    var modal_container2 = document.getElementById('modal_container2');
+    var close2 = document.getElementById('btn_cancelar2');
 
-open2.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal_container2.classList.add('show');
-},false);
-
-
-close2.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal_container2.classList.remove('show');
-},false);
+    //Recorremos el arreglo de elementos con el name openModal2
+    for(var i=0; i<listOpen2.length; i++){
+        
+        listOpen2[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            modal_container2.classList.add('show');
+        },false);
+    }
+    
+    close2.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal_container2.classList.remove('show');
+    },false); 
+}
 
 
 
 
 //INVOCACION DE FUNCIONES
-eventoPopUpInfoEportafolio();
-dibujarGraficoPerfilamientoPandora();
+eventoPopUpCompartirEportafolio();
+//dibujarGraficoPerfilamientoPandora();
